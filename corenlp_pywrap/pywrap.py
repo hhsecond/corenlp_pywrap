96979b6c3ad870b45744b53ecc0ad674245b573d
Sherin Thomas <sherinct@live.com>
2016-08-02 16:27:16 +0000

    add support for all output format, basic function now returning request object

--- corenlp_pywrap/pywrap.py
+++ corenlp_pywrap/pywrap.py
@@ -13,7 +13,7 @@ class CoreNLP:
     annotator_full_list = ["tokenize", "cleanxml", "ssplit", "pos", 
     "lemma", "ner", "regexner", "truecase", "parse", "depparse", "dcoref", 
     "relation", "natlog", "quote"]
-    url = 'http://127.0.0.1:9000'
+    url = 'http://corenlp.run'
 
     def __init__(self, url=url, annotator_list=annotator_full_list):        
         assert url.upper().startswith('HTTP'), 'url string should be prefixed with http'
@@ -22,33 +22,34 @@ class CoreNLP:
         assert isinstance(annotator_list, list), "annotators can be passed only as a python list"
         if len(annotator_list) == 14:
             root.info('Using all the annotators')
+
         self.annotator_list = annotator_list
-
-
-    def basic(self, data, out_format='json'):
-        s_string = '/?properties={"annotators": "'
-        anot_string = ','.join(self.annotator_list)
-        f_string = '", "outputFormat": "' + out_format + '"}' 
-        current_url = self.url + s_string + anot_string + f_string
-
+        
         common=set(self.annotator_list).intersection(self.annotator_full_list)
         not_suprtd_elem = set(self.annotator_list) - common
         assertion_error = 'annotator not supported: ' + str(not_suprtd_elem)
         assert not not_suprtd_elem, assertion_error
 
+
+    def basic(self, data, out_format='json', serializer=''):
+        format_list = ['JSON', 'XML', 'TEXT', 'SERIALIZED']
+        assert out_format.upper() in format_list, 'output format not supported, check stanford doc'
+        
+        if out_format.upper() == 'SERIALIZED' and not serializer:
+            root.info('Default Serializer is using - edu.stanford.nlp.pipeline.CustomAnnotationSerializer')
+            serializer = 'edu.stanford.nlp.pipeline.CustomAnnotationSerializer'
+            
+        s_string = '/?properties={"annotators": "'
+        anot_string = ','.join(self.annotator_list)
+        f_string = '", "outputFormat": "' + out_format + '"}' 
+        current_url = self.url + s_string + anot_string + f_string
+
         assert isinstance(data, str) and data, 'Enter valid string input'
 
         try:
-            r = requests.post(current_url, data, headers={'Connection': 'close'})
+            server_out = requests.post(current_url, data, headers={'Connection': 'close'})
         except requests.exceptions.ConnectionError:
             logging.error('Connection Error, check you have server running')
             raise Exception('Check your CoreNLP Server status \n'
-                'if not sure, Check the pywrap doc for Server instantiation')
-        if 'json' in out_format:
-            server_out = r.json()
-        elif 'text' in out_format:
-            server_out = r.text
-        else:
-            root.warning('out put format given is not supported, used json instead')
-            server_out = r.json()
+                'if not sure, Check the pywrap doc for Server instantiation \n')
         return server_out
