04f26ee107c1d2ad1dbc1f76d0cbe9f2d090b7da
hhsecond <sherinct@live.com>
2016-08-06 20:16:57 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools import setup
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return '\n'.join(f.readlines())
 
 setup(
     name = "corenlp_pywrap",
-    version = "0.0.6",
+    version = "0.0.9",
     author = "hhsecond",
     author_email = "sherinct@live.com",
     description = ("A powerful python wraper for Stanford CoreNLP"),
@@ -18,10 +19,11 @@ setup(
     keywords = "stanford corenlp wrapper",
     install_requires=['requests'],
     url = "https://www.github.com/hhsecond/corenlp_pywrap",
+    download_url = "https://www.github.com/hhsecond/corenlp_pywrap/tarball/0.0.9",
     packages=['corenlp_pywrap'],
-    long_description='this is a long description',
+    long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
