08d5ec44346e10a3098ad6d0fc8a70a9b5a04c67
hhsecond <sherinct@live.com>
2016-08-13 14:27:00 +0530

    1.0.3

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 import os
 from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 setup(
     name = "corenlp_pywrap",
-    version = "1.0.2",
+    version = "1.0.3",
     author = "hhsecond",
     author_email = "sherinct@live.com",
     description = ("A powerful python wraper for Stanford CoreNLP"),
@@ -15,7 +10,7 @@ setup(
     keywords = "stanford corenlp wrapper",
     install_requires=['requests'],
     url = "https://www.github.com/hhsecond/corenlp_pywrap",
-    download_url = "https://www.github.com/hhsecond/corenlp_pywrap/tarball/1.0.2",
+    download_url = "https://www.github.com/hhsecond/corenlp_pywrap/tarball/1.0.3",
     packages=['corenlp_pywrap'],
     long_description='Production Ready version equiped with basic'\
         'output fetch of stanfornlp and custom arrange function, '\
